e3dd8de01d1c2e906ea29ce178fe264710ca34c5
Jeffrei <hjefrei@gmail.com>
2021-03-03 13:53:42 -0400

    Se agregaron los modulos proveerores,inentario,producto,categoria

--- src/models/index.js
+++ src/models/index.js
@@ -19,5 +19,9 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 db.test = require("./test.model.js")(sequelize, Sequelize);
+db.inventario = require("./inventario.model.js")(sequelize, Sequelize);
+db.productos = require("./producto.model.js")(sequelize, Sequelize);
+db.proveedor = require("./proveedores.model.js")(sequelize, Sequelize);
+db.categorias = require("./categoria.model.js")(sequelize, Sequelize);
 
 module.exports = db;
---
+++ src/models/inventario.model.js
@@ -0,0 +1,44 @@
+module.exports = (sequelize, Sequelize) => {
+    const Inventario = sequelize.define('inventario', {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        id_proveedor: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        id_producto: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        cantidad:{
+            type: Sequelize.INTEGER(50),
+            allowNull: false
+        },
+        fechaVencimiento: {
+            type: Sequelize.DATE
+        },
+        cantidadMinima: {
+            type: Sequelize.INTEGER(50),
+            allowNull: false
+        },
+        fechaEntrada: {
+            type: Sequelize.DATE,
+            defaultValue: Sequelize.fn('NOW')
+        }
+    },
+    {
+        timestamps: false
+    });
+    
+    Inventario.associate = (models) => {
+        Inventario.hasMany(models.Proveedor);
+        Inventario.hasMany(models.Produtos);
+        Inventario.belosngsTo(models.Produtos);
+    };
+
+
+}
