c58cb5418d26b2145979b408bbc41fb9f69c0577
Richard Hernandez <richard.hernandez@externos.toc.cl>
2021-03-17 10:40:01 -0400

    changing front-end

--- app/src/index.js
+++ app/src/index.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import indexRoutes from './routes/index.jsx';
-import {  Route, Switch } from 'react-router-dom';
-import { HashRouter } from 'react-router-dom'
-
-import './assets/scss/style.css';
-
+import './index.css';
+import App from './App';
 
 
 ReactDOM.render(
-  <HashRouter>
-  
-    <Switch>
-      {indexRoutes.map((prop, key) => {
-        return <Route path={prop.path} key={key} component={prop.component} />;
-      })}
-    </Switch>
-  
-  </HashRouter>
-  ,document.getElementById('root')); 
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
