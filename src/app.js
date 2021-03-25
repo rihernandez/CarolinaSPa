cf08b5edec5f479a756144a81ebc51b562474952
Richard Hernandez <richard.hernandez@externos.toc.cl>
2021-03-25 00:49:03 -0400

    refactoring

--- src/app.js
+++ src/app.js
@@ -1,56 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const db = require("./models");
-const cors = require('cors');
-require('dotenv').config()
+import React from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Navigation from './components/Navigation'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
 
+function App() {
+  return (
+    <Router>
+      <Navigation/>
+      <div className="container p-4">
+        <Route path="/"/>
+      </div>
+    </Router>
+  );
+}
 
-// const port = 5000;
-
-// connect to database
-
- db.sequelize.sync();
-
-// db.sequelize.sync({ force: true }).then(() => {
-//     console.log("Drop and re-sync db.");
-//   });
-
-// configure middleware
-
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
-
-app.use(cors(corsOptions));
-
-app.set('port', process.env.PORT); 
-
-console.log("TEST ", app.get('port'))
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-// parse requests of content-type - application/json
-app.use(bodyParser.json());
-
-
-
-// routes for the app
-require("./routes/test.route")(app);
-require("./routes/proveedor.route")(app);
-require("./routes/categorias.route")(app);
-require("./routes/inventario.route")(app);
-require("./routes/producto.route")(app);
-require("./routes/facturaservicio.route")(app);
-require("./routes/servicios.route")(app);
-require("./routes/facturaserviciodet.route")(app);
-require("./routes/perfiles.route")(app);
-require("./routes/citas.route")(app);
-require("./routes/estadocita.route")(app);
-require("./routes/clientes.route")(app);
-require("./routes/estadofactura.route")(app);
-
-
-// set the app to listen on the port
-app.listen( app.get('port'), () => {
-    console.log("Server running on port:" +  app.get('port'));
-});
+export default App;
