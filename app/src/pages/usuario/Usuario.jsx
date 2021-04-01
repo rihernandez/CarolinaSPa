f650ab3efbc3367446bfe9d571b7614f33eb300a
Daviel Jimenez <Daviel050399@gmail.com>
2021-03-31 21:32:20 -0400

    CRUD Usuario Done

--- app/src/pages/usuario/Usuario.jsx
+++ app/src/pages/usuario/Usuario.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import GenericTable from "../../components/GenericTable";
 import { call } from "../../utils/api";
+import swal from "sweetalert";
+import { getUser } from "../../utils/auth";
 
 const headers = [
   "ID Usuario",
@@ -15,14 +17,53 @@ const fields = ["ID_Usuario", "Nombre", "Apellidos", "Email", "NombreRol"];
 const Usuario = ({ history }) => {
   const [usuarios, setUsuarios] = React.useState([]);
 
+  const setFilteredUsuarios = (users) => {
+    const filtered = users.filter((u) => {
+      return u.ID_Usuario !== getUser().id;
+    });
+    setUsuarios(filtered);
+  };
+
   const getUsuarios = React.useCallback(async () => {
     try {
       const res = await call("get", "/usuario");
-      if (Array.isArray(res)) setUsuarios(res);
+      if (Array.isArray(res)) setFilteredUsuarios(res);
     } catch (e) {
       console.log("Error fetching facturas", e);
     }
   }, []);
+
+  const handleDeleteUsuario = async (id) => {
+    const res = await swal("Estas seguro que deseas eliminar este usuario ?", {
+      buttons: ["Cancelar!", "Si"],
+    });
+
+    if (!res) return;
+    try {
+      await call("delete", `usuario/${id}`);
+      removeUsuarioFromState(id);
+      swal(
+        "Usuario eliminado con exito",
+        `Presiona "OK" para continuar`,
+        "success"
+      );
+    } catch (e) {
+      console.log("Error elimando el usuario", e);
+      swal(
+        "El usuarioi no puede ser eliminado debido a que esta en uso",
+        `Presiona "OK" para continuar`,
+        "error"
+      );
+    }
+  };
+
+  const removeUsuarioFromState = (id) => {
+    const filtered = usuarios.filter((us) => {
+      return us.ID_Usuario !== id;
+    });
+
+    setUsuarios(filtered);
+  };
 
   React.useEffect(() => {
     getUsuarios();
@@ -32,15 +73,27 @@ const Usuario = ({ history }) => {
     <div className="mt-5">
       <div className="d-flex justify-content-between  mb-2">
         <div>
-          <h3>Usuario</h3>
+          <h3>Usuarios</h3>
         </div>
         <div style={{ width: 300 }}>
-          <button type="button" className="btn btn-primary">
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={() => history.replace("/usuario/detalle")}
+          >
             Crear Usuario
           </button>
         </div>
       </div>
-      <GenericTable headers={headers} fields={fields} data={usuarios} />
+      <GenericTable
+        headers={headers}
+        fields={fields}
+        data={usuarios}
+        onRowSelected={(id) => history.replace(`/usuario/detalle/${id}`)}
+        onDeleteRow={handleDeleteUsuario}
+        idName="ID_Usuario"
+        showDelete={true}
+      />
     </div>
   );
 };
