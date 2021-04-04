45932a1b956152573381cc0f62ea06bcf754df93
Daviel Jimenez <Daviel050399@gmail.com>
2021-04-03 23:46:14 -0400

    Fixing Create Category

--- app/src/components/CreateCategoria.js
+++ app/src/components/CreateCategoria.js
@@ -84,84 +84,81 @@ class CreateCategoria extends Component {
     };
 
     render() {
-        return ( <
-            div className = "row" >
-            <
-            div className = "col-md-4" >
-            <
-            div className = "card card-body" >
-            <
-            h3 > Crear Nueva Categoria < /h3>{" "} <
-            form onSubmit = { this.onSubmit } >
-            <
-            div className = "form-group" >
-            <
-            label className = "text-muted" > Categoria * < /label>{" "} <
-            input type = "text"
-            className = "form-control"
-            name = "categoria"
-            value = { this.state.categoria }
-            onChange = { this.onChange }
-            />{" "} <
-            /div>{" "} <
-            div className = "form-group" >
-            <
-            label className = "text-muted" > Descripcion Categoria * < /label>{" "} <
-            textarea name = "descripcion"
-            value = { this.state.descripcion }
-            onChange = { this.onChange }
-            className = "form-control" >
-            < /textarea>{" "} <
-            /div>{" "} <
-            button type = "submit"
-            className = "btn btn-success btn-block" >
-            Guardar { " " } <
-            /button>{" "} <
-            /form>{" "} <
-            /div>{" "} <
-            /div>{" "} <
-            div className = "col-md-8" > { " " } {
-                this.state.editing ? ( <
-                    div > < /div>
-                ) : ( <
-                    table className = "table table-hover" >
-                    <
-                    thead >
-                    <
-                    tr >
-                    <
-                    th > Categoria < /th> <th> Descripcion </th > { " " } <
-                    th > Acciones < /th>{" "} <
-                    /tr>{" "} <
-                    /thead>{" "} <
-                    tbody > { " " } {
-                        this.state.categorias.map((categoria) => ( <
-                            tr key = { categoria.id_Categoria }
-                            onDoubleClick = {
-                                () =>
-                                this.deleteCategoria(categoria.id_Categoria)
-                            } >
-                            <
-                            td > { categoria.categoria } < /td>{" "} <
-                            td > { categoria.descripcion } < /td>{" "} <
-                            td >
-                            <
-                            Link className = "btn btn-sm  btn-warning"
-                            to = { "/edit-categoria/" + categoria.id_Categoria } >
-                            Editar { " " } <
-                            /Link>{" "} <
-                            /td>{" "} <
-                            /tr>
-                        ))
-                    } { " " } <
-                    /tbody>{" "} <
-                    /table>
-                )
-            } { " " } <
-            /div>{" "} <
-            /div>
-        );
+        return (
+            <div className="row">
+                <div className="col-md-4">
+                    <div className="card card-body">
+                        <h3>Crear Nueva Categoria</h3>
+                        <form onSubmit={this.onSubmit}>
+                            <div className="form-group">
+                                <label className="text-muted">Categoria *</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    name="categoria"
+                                    value={this.state.categoria}
+                                    onChange={this.onChange}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label className="text-muted">Descripcion Categoria *</label>
+                                <textarea
+                                    name="descripcion"
+                                    value={this.state.descripcion}
+                                    onChange={this.onChange}
+                                    className="form-control"
+                                >
+                                </textarea>
+                            </div>
+                            <button type="submit" className="btn btn-success btn-block">
+                                Guardar
+                            </button>
+                        </form>
+                    </div>
+                </div>
+                <div className="col-md-8">
+                    {
+                        this.state.editing
+                            ? <div></div>
+                            : <table className="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Categoria</th>
+                                        <th>Descripcion</th>
+                                        <th>Acciones</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        this.state.categorias.map(categoria => (
+                                            <tr
+                                                key={categoria.id_Categoria}
+                                                onDoubleClick={() => this.deleteCategoria(categoria.id_Categoria)}
+                                            >
+                                                <td>{categoria.categoria}</td>
+                                                <td>{categoria.descripcion}</td>
+                                                <td>
+                                                    <Link
+                                                        className="btn btn-sm  btn-warning"
+                                                        to={"/edit-categoria/" + categoria.id_Categoria}
+                                                    >
+                                                        Editar
+                                                    </Link>
+                                                </td>
+                                            </tr>
+                                        ))
+                                    }
+                                </tbody>
+                            </table>
+                    }
+                </div>
+            </div>
+        )
     }
+
+
 }
+
+
 
 export default CreateCategoria;
